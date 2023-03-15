36eb9adc6e7f46111469acee7f7bf47b40fde8f0
Scott Wilson <wilsco56@gmail.com>
2023-03-14 20:12:02 -0400

    Update three_day_rolling_avg.sql

--- three_day_rolling_avg.sql
+++ three_day_rolling_avg.sql
@@ -1,16 +1,25 @@
-WIth lag_results AS (
+--group transactions by day
+WITH sum_by_day AS(
+  SELECT 
+  	date(transaction_time) AS transaction_day,
+  	sum(transaction_amount) AS sum_transaction_amount
+  FROM
+  	transactions
+  GROUP BY 1),
+--get previous day and previous 2 day
+lag_results AS (
 Select
-  	transaction_time,
-  	Transaction_amount,
-	lag(transaction_amount,1) OVER(ORDER BY transaction_time ASC) AS previous_day_transaction,
-    lag(transaction_amount,2) OVER(ORDER BY transaction_time ASC) AS previous_2_day_transaction
+  	transaction_day,
+  	sum_transaction_amount,
+	lag(sum_transaction_amount,1) OVER(ORDER BY transaction_day ASC) AS previous_day_transaction,
+    	lag(sum_transaction_amount,2) OVER(ORDER BY transaction_day ASC) AS previous_2_day_transaction
 FROM 
-  transactions
+  sum_by_day
 ORDER BY
   1 ASC
         )
 SELECT 
-	transaction_time,
-    (transaction_amount+previous_day_transaction+previous_2_day_transaction)/ 3 as three_day_rolling_avg
+	transaction_day, 		
+	(sum_transaction_amount+previous_day_transaction+previous_2_day_transaction)/ 3 as three_day_rolling_avg
 FROM 
 	lag_results
